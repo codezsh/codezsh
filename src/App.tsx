d10abbfc95c17b385dd1db9698e33b4713afc24f
= <=>
2025-05-17 12:30:32 +0530

    started building website

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+
+function App() {
+  return (
+    <div className="flex justify-center items-center relative w-screen h-screen" id="main">
+      
+      <div className="text-center">
+        <h1 className="text-white text-3xl capitalize relative z-10 p-4">
+          under construction.. 🔥
+        </h1>
+
+        <p className="text-white z-10 relative">I am currently working on this website 🚀</p>
+
+        <button className="bg-white text-black relative p-2 rounded-sm w-32 cursor-pointer m-4" onClick={()=>window.location.href="https://github.com/codezsh"}>Github 😺</button>
+
+        <button className="bg-white text-black relative p-2 rounded-sm w-32 cursor-pointer m-4" onClick={()=>window.location.href="https://medium.com/@codezsh"}>Blog 📄</button>
+
+
+      </div>
+    
+    </div>
+
+  )
+}
+
+export default App
